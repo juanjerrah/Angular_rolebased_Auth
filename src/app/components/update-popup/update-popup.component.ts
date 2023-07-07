40fe1ee603228279e4a87f37a3b5dc03ec7ebbf1
Juanjerrah <juan.oliveira@avonale.com.br>
2023-07-07 12:50:25 -0300

    vAI

---
+++ src/app/components/update-popup/update-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-popup',
+  template: `
+    <p>
+      update-popup works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UpdatePopupComponent {
+
+}
---
+++ src/app/pages/user-listing/user-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-listing',
+  template: `
+    <p>
+      user-listing works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UserListingComponent {
+
+}
