6b03c30a18768fbd022507459e2743d2593a6ec4
Juanjerrah <juan.oliveira@avonale.com.br>
2023-07-07 14:16:55 -0300

    feat: style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserListingComponent } from './pages/user-listing/user-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  { path:'register', component:RegisterComponent },
+  { path:'login', component:LoginComponent },
+  { path:'user', component:UserListingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
 
