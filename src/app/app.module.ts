18d3944f08e352a0ea51d2c13af6bdac4c982469
Juanjerrah <juan.oliveira@avonale.com.br>
2023-07-07 12:49:18 -0300

    Creating main pages and components stucture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material.module';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserListingComponent } from './pages/user-listing/user-listing.component';
+import { UpdatePopupComponent } from './components/update-popup/update-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    UserListingComponent,
+    UpdatePopupComponent
   ],
   imports: [
     BrowserModule,
